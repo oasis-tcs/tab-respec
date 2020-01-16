287c3b4cfa05b79021c450fb54981f997e35c481
Nick Crossley <ndjc@ncrossley.com>
2020-01-16 09:16:43 -0800

    Commit results of build

--- builds/oasis/respec-oasis-common-1.0.2.js
+++ builds/oasis/respec-oasis-common-1.0.2.js
@@ -6595,7 +6595,7 @@ define(
         ,   "PR":       "W3C Proposed Recommendation"
         ,   "PER":      "W3C Proposed Edited Recommendation"
         ,   "REC":      "W3C Recommendation"
-        };
+       };
         var stringifyRef = function(ref) {
             if (typeof ref === "string") return ref;
             var output = "";
@@ -6692,11 +6692,11 @@ define(
         return {
             stringifyRef: stringifyRef,
             run:    function (conf, doc, cb, msg) {
-                msg.pub("start", "core/biblio");
+                msg.pub("start", "oasis/biblio");
                 var refs = getRefKeys(conf)
                 ,   localAliases = []
                 ,   finish = function () {
-                        msg.pub("end", "core/biblio");
+                        msg.pub("end", "oasis/biblio");
                         cb();
                     }
                 ;
@@ -6711,7 +6711,7 @@ define(
                                 .concat(refs.informativeReferences)
                                 .concat(localAliases);
                 if (refs.length) {
-                    var url = "https://labs.w3.org/specrefs/bibrefs?refs=" + refs.join(",");
+                    var url = "https://api.specref.org/bibrefs?refs=" + refs.join(",");
                     $.ajax({
                         dataType:   "json"
                     ,   url:        url
@@ -6726,6 +6726,8 @@ define(
                         }
                     ,   error:      function (xhr, status, error) {
                             msg.pub("error", "Error loading references from '" + url + "': " + status + " (" + error + ")");
+                            conf.biblio = conf.localBiblio;
+                            bibref(conf, msg);
                             finish();
                         }
                     });
@@ -9391,188 +9393,11 @@ define(
     }
 );
 
-
-// Module core/biblio
-// Handles bibliographic references
-// Configuration:
-//  - localBiblio: override or supplement the official biblio with your own.
-
-define(
-    'core/biblio',[],
-    function () {
-        var getRefKeys = function (conf) {
-            var informs = conf.informativeReferences
-            ,   norms = conf.normativeReferences
-            ,   del = []
-            ,   getKeys = function (obj) {
-                    var res = [];
-                    for (var k in obj) res.push(k);
-                    return res;
-                }
-            ;
-            for (var k in informs) if (norms[k]) del.push(k);
-            for (var i = 0; i < del.length; i++) delete informs[del[i]];
-            return {
-                informativeReferences: getKeys(informs),
-                normativeReferences: getKeys(norms)
-            };
-        };
-        var REF_STATUSES = {
-            "NOTE":     "W3C Note"
-        ,   "WG-NOTE":  "W3C Working Group Note"
-        ,   "ED":       "W3C Editor's Draft"
-        ,   "FPWD":     "W3C First Public Working Draft"
-        ,   "WD":       "W3C Working Draft"
-        ,   "LCWD":     "W3C Last Call Working Draft"
-        ,   "CR":       "W3C Candidate Recommendation"
-        ,   "PR":       "W3C Proposed Recommendation"
-        ,   "PER":      "W3C Proposed Edited Recommendation"
-        ,   "REC":      "W3C Recommendation"
-        };
-        var stringifyRef = function(ref) {
-            if (typeof ref === "string") return ref;
-            var output = "";
-            if (ref.authors && ref.authors.length) {
-                output += ref.authors.join("; ");
-                if (ref.etAl) output += " et al";
-                output += ". ";
-            }
-            if (ref.href) output += '<a href="' + ref.href + '"><cite>' + ref.title + "</cite></a>. ";
-            else output += '<cite>' + ref.title + '</cite>. ';
-            if (ref.date) output += ref.date + ". ";
-            if (ref.status) output += (REF_STATUSES[ref.status] || ref.status) + ". ";
-            if (ref.href) output += 'URL: <a href="' + ref.href + '">' + ref.href + "</a>";
-            return output;
-        };
-        var bibref = function (conf, msg) {
-            // this is in fact the bibref processing portion
-            var badrefs = {}
-            ,   refs = getRefKeys(conf)
-            ,   informs = refs.informativeReferences
-            ,   norms = refs.normativeReferences
-            ,   aliases = {}
-            ;
-
-            if (!informs.length && !norms.length && !conf.refNote) return;
-            var $refsec = $("<section id='references' class='appendix'><h2>References</h2></section>").appendTo($("body"));
-            if (conf.refNote) $("<p></p>").html(conf.refNote).appendTo($refsec);
-
-            var types = ["Normative", "Informative"];
-            for (var i = 0; i < types.length; i++) {
-                var type = types[i]
-                ,   refs = (type == "Normative") ? norms : informs;
-                if (!refs.length) continue;
-                var $sec = $("<section><h3></h3></section>")
-                                .appendTo($refsec)
-                                .find("h3")
-                                    .text(type + " references")
-                                .end()
-                                ;
-                $sec.makeID(null, type + " references");
-                refs.sort();
-                var $dl = $("<dl class='bibliography'></dl>").appendTo($sec);
-                if (conf.doRDFa !== false) $dl.attr("about", "");
-                for (var j = 0; j < refs.length; j++) {
-                    var ref = refs[j];
-                    $("<dt></dt>")
-                        .attr({ id:"bib-" + ref })
-                        .text("[" + ref + "]")
-                        .appendTo($dl)
-                        ;
-                    var $dd = $("<dd></dd>").appendTo($dl);
-                    if (this.doRDFa !== false) {
-                        if (type === "Normative") $dd.attr("rel", "dcterms:requires");
-                        else $dd.attr("rel", "dcterms:references");
-                    }
-                    var refcontent = conf.biblio[ref]
-                    ,   circular = {}
-                    ,   key = ref;
-                    circular[ref] = true;
-                    while (refcontent && refcontent.aliasOf) {
-                        if (circular[refcontent.aliasOf]) {
-                            refcontent = null;
-                            msg.pub("error", "Circular reference in biblio DB between [" + ref + "] and [" + key + "].");
-                        }
-                        else {
-                            key = refcontent.aliasOf;
-                            refcontent = conf.biblio[key];
-                            circular[key] = true;
-                        }
-                    }
-                    aliases[key] = aliases[key] || [];
-                    if (aliases[key].indexOf(ref) < 0) aliases[key].push(ref);
-                    if (refcontent) {
-                        $dd.html(stringifyRef(refcontent) + "\n");
-                    }
-                    else {
-                        if (!badrefs[ref]) badrefs[ref] = 0;
-                        badrefs[ref]++;
-                        $dd.html("<em style='color: #f00'>Reference not found.</em>\n");
-                    }
-                }
-            }
-            for (var k in aliases) {
-                if (aliases[k].length > 1) {
-                    msg.pub("warn", "[" + k + "] is referenced in " + aliases[k].length + " ways (" + aliases[k].join(", ") + "). This causes duplicate entries in the reference section.");
-                }
-            }
-            for (var item in badrefs) {
-                if (badrefs.hasOwnProperty(item)) msg.pub("error", "Bad reference: [" + item + "] (appears " + badrefs[item] + " times)");
-            }
-        };
-        
-        return {
-            stringifyRef: stringifyRef,
-            run:    function (conf, doc, cb, msg) {
-                msg.pub("start", "core/biblio");
-                var refs = getRefKeys(conf)
-                ,   localAliases = []
-                ,   finish = function () {
-                        msg.pub("end", "core/biblio");
-                        cb();
-                    }
-                ;
-                if (conf.localBiblio) {
-                    for (var k in conf.localBiblio) {
-                        if (typeof conf.localBiblio[k].aliasOf !== "undefined") {
-                            localAliases.push(conf.localBiblio[k].aliasOf);
-                        }
-                    }
-                }
-                refs = refs.normativeReferences
-                                .concat(refs.informativeReferences)
-                                .concat(localAliases);
-                if (refs.length) {
-                    var url = "https://labs.w3.org/specrefs/bibrefs?refs=" + refs.join(",");
-                    $.ajax({
-                        dataType:   "json"
-                    ,   url:        url
-                    ,   success:    function (data) {
-                            conf.biblio = data || {};
-                            // override biblio data
-                            if (conf.localBiblio) {
-                                for (var k in conf.localBiblio) conf.biblio[k] = conf.localBiblio[k];
-                            }
-                            bibref(conf, msg);
-                            finish();
-                        }
-                    ,   error:      function (xhr, status, error) {
-                            msg.pub("error", "Error loading references from '" + url + "': " + status + " (" + error + ")");
-                            finish();
-                        }
-                    });
-                }
-                else finish();
-            }
-        };
-    }
-);
-
 // Module ui/search-specref
 // Search Specref database
 
 define(
-    'ui/search-specref',["jquery", "core/biblio"],
+    'ui/search-specref',["jquery", "oasis/biblio"],
     function ($, biblio) {
         function pluralize (count, sing, plur) {
             return count + ' ' + (count == 1 ? sing : plur);
@@ -9611,7 +9436,7 @@ define(
                 $halp.find("form").on("submit", function() {
                     $status.html("Searching…");
                     var query = $search.val();
-                    $.getJSON("https://labs.w3.org/specrefs/search-refs", { q: query }).then(function(json) {
+                    $.getJSON("https://api.specref.org/specrefs/search-refs", { q: query }).then(function(json) {
                         var output = buildResults(json);
                         $results.html(output.html);
                         $status.html(msg(query, output.count));
