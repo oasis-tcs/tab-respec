e4443c08ce8bd986bf712d3ab9b58490b6694a49
Nick Crossley <ndjc@ncrossley.com>
2020-06-09 08:24:27 -0700

    Catch up with master where possible

--- js/conformance.js
+++ js/conformance.js
@@ -2,23 +2,23 @@
 // Handle the conformance section outline - details filled in later with conformance-table.js.
 
 define(
-    [],
-    function() {
-        return {
-        run : function(conf, doc, cb, msg) {
-            msg.pub("start", "conformance");
-            var conform = $("#conformance");
-            var clauses = $(".conformance");
-            conf.conformanceIntro = conform.find(":header ~").length;
+	[],
+	function() {
+	    return {
+		run : function(conf, doc, cb, msg) {
+		    msg.pub("start", "conformance");
+		    var conform = $("#conformance");
+		    var clauses = $(".conformance");
+		    conf.conformanceIntro = conform.find(":header ~").length;
 
-            if (!conf.isNoTrack && conf.conformanceIntro <= 0
-                && (conf.noConformanceTable || clauses.length <= 0)) {
-                msg.pub("error","A conformance paragraph or table is required for your type of document.");
-            } else if (conform.find(":header").length <= 0) {
-                conform.append("<h2>Conformance</h2>");
-            }
-            msg.pub("end", "conformance");
-            cb();
-        }
-        };
-    });
+		    if (!conf.isNoTrack && conf.conformanceIntro <= 0
+			    && (conf.noConformanceTable || clauses.length <= 0)) {
+			msg.pub("error","A conformance paragraph or table is required for your type of document.");
+		    } else if (conform.find(":header").length <= 0) {
+			conform.append("<h2>Conformance</h2>");
+		    }
+		    msg.pub("end", "conformance");
+		    cb();
+		}
+	    };
+	});
